d0a253dd378fc11e9d2fd5afb04be1075e87a10a
KemalGencer <gencer0055@gmail.com>
2025-06-27 15:18:53 +0300

    Hide debug banner
    The debug banner in the top-right corner of the app has been hidden by setting `debugShowCheckedModeBanner` to `false` in the `MaterialApp` widget.

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Beykoz App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
