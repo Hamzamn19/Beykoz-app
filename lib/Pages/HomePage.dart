42245d5e21125137d5df2d55026d12d141e11754
KemalGencer <gencer0055@gmail.com>
2025-07-01 22:15:36 +0300

    feat: Make announcement cards tappable and navigate to webviews
    - Modified `_buildAnnouncementCard` to accept `context`, `imagePath`, and `url` parameters.
    - Implemented navigation to a `WebViewPage` when an announcement card is tapped, displaying the content of the provided `url`.
    - Updated the two announcement cards on the home page to use the new `_buildAnnouncementCard` with specific images and URLs.
    - Added new image assets (`mezuniyettoreni.jpg` and `yazogretimi.jpg`) for the announcement cards.
    - Ensured `WebViewPage` state updates (`_isLoading`, `_canGoBack`, `_canGoForward`) are safely performed after the build frame is complete using `WidgetsBinding.instance.addPostFrameCallback`.
    - Minor code cleanup in `HomePage.dart`.

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:beykoz/Pages/AttendancePage.dart';
-import 'package:beykoz/Pages/ProfilePage.dart'; // Make sure this path is correct
+import 'package:beykoz/Pages/ProfilePage.dart';
 import 'package:beykoz/Pages/AllFeaturesPage.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // will be used for the logo
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // Main widget containing the home screen and Bottom Navigation Bar logic
 class HomeScreen extends StatefulWidget {
@@ -115,9 +115,19 @@ class DesignedHomePage extends StatelessWidget {
               const SizedBox(height: 24),
               _buildSectionTitle('DUYURULAR'),
               const SizedBox(height: 12),
-              _buildAnnouncementCard(),
+
+              // DEĞİŞİKLİK 1: Kartlar artık tıklanabilir ve ilgili linklere yönlendirir
+              _buildAnnouncementCard(
+                context: context,
+                imagePath: 'assets/images/mezuniyettoreni.jpg',
+                url: 'https://www.beykoz.edu.tr/haber/5581-2025-mezuniyet-toreni',
+              ),
               const SizedBox(height: 16),
-              _buildAnnouncementCard(),
+              _buildAnnouncementCard(
+                context: context,
+                imagePath: 'assets/images/yazogretimi.jpg',
+                url: 'https://www.beykoz.edu.tr/haber/5616-2024-2025-yaz-ogretiminde-acilabilecek-dersler-duyurusu',
+              ),
             ],
           ),
         ),
@@ -217,6 +227,7 @@ class DesignedHomePage extends StatelessWidget {
   }
 
   Widget _buildFrequentlyUsedGrid() {
+    // ... (This part of the code is unchanged)
     final List<Map<String, dynamic>> frequentlyUsed = [
       {
         'label': 'Transkript',
@@ -259,7 +270,6 @@ class DesignedHomePage extends StatelessWidget {
         'icon': FontAwesomeIcons.calendarCheck, // DEĞİŞTİRİLDİ
       },
     ];
-
     return GridView.builder(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
@@ -327,20 +337,33 @@ class DesignedHomePage extends StatelessWidget {
     );
   }
 
-  Widget _buildAnnouncementCard() {
-    return Container(
-      height: 150,
-      decoration: BoxDecoration(
-        color: cardColor,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: const Center(
-        child: Text(
-          'L',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 48,
-            fontWeight: FontWeight.bold,
+  // DEĞİŞİKLİK 2: _buildAnnouncementCard metodu artık tıklanabilir ve dinamik.
+  Widget _buildAnnouncementCard({
+    required BuildContext context,
+    required String imagePath,
+    required String url,
+  }) {
+    return InkWell(
+      onTap: () {
+        // Tıklandığında WebViewPage'i aç ve ilgili URL'i gönder.
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => WebViewPage(
+              url: url,
+            ),
+          ),
+        );
+      },
+      borderRadius: BorderRadius.circular(16), // Tıklama efektinin köşelerini yuvarlak yapar
+      child: Container(
+        height: 150,
+        decoration: BoxDecoration(
+          color: cardColor, // Resim yüklenemezse görünecek arka plan rengi
+          borderRadius: BorderRadius.circular(16),
+          image: DecorationImage(
+            image: AssetImage(imagePath), // Gösterilecek resim
+            fit: BoxFit.cover, // Resmin tüm alanı kaplamasını sağlar
           ),
         ),
       ),
@@ -349,6 +372,7 @@ class DesignedHomePage extends StatelessWidget {
 }
 
 // --- WEBVIEW PAGE ---
+// ... (This part of the code is unchanged)
 class WebViewPage extends StatefulWidget {
   final String url;
   final String? username;
@@ -360,7 +384,6 @@ class WebViewPage extends StatefulWidget {
     this.password,
     super.key,
   });
-
   @override
   State<WebViewPage> createState() => _WebViewPageState();
 }
@@ -371,10 +394,8 @@ class _WebViewPageState extends State<WebViewPage>
   bool _isLoading = true;
   bool _canGoBack = false;
   bool _canGoForward = false;
-
   @override
   bool get wantKeepAlive => true;
-
   @override
   void initState() {
     super.initState();
@@ -384,7 +405,6 @@ class _WebViewPageState extends State<WebViewPage>
       ..setNavigationDelegate(
         NavigationDelegate(
           onPageStarted: (String url) {
-            // FIX: Safely update state after the build frame is complete
             WidgetsBinding.instance.addPostFrameCallback((_) {
               if (mounted) {
                 setState(() {
@@ -394,7 +414,6 @@ class _WebViewPageState extends State<WebViewPage>
             });
           },
           onPageFinished: (String url) async {
-            // FIX: Safely update state after the build frame
             WidgetsBinding.instance.addPostFrameCallback((_) async {
               await _updateNavigationState();
               if (mounted) {
@@ -411,7 +430,7 @@ class _WebViewPageState extends State<WebViewPage>
                 username = username.substring(0, atIndex);
               }
               final loginJs =
-                  '''
+              '''
                 (function() {
                   var username = "$username";
                   var password = "${widget.password}";
@@ -437,7 +456,6 @@ class _WebViewPageState extends State<WebViewPage>
             }
           },
           onWebResourceError: (WebResourceError error) {
-            // FIX: Safely update state and show SnackBar after the build frame
             WidgetsBinding.instance.addPostFrameCallback((_) {
               if (mounted) {
                 setState(() {
